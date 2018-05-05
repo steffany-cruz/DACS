7e4730988d15c6f76b27069ea97a6fa96d36f9e0
Steffany <steffany.cruz@univille.br>
2018-05-04 21:08:37 -0300

    Add files via upload

---
+++ wpsteffany/steffany/header.php
@@ -0,0 +1,19 @@
+
+<link rel="stylesheet" type="text/css" href="/link/to/.css" media="all">
+ <link rel="profile" href="http://gmpg.org/xfn/11" />
+		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+		<title><?php wp_title(''); ?></title>
+		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); wp_head(); ?>
+ 
+	    <link type="text/css" rel="stylesheet" href="style.css" />
+	    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	    <title> Site da Steffany </title> 
+
+		<div class="fundoBarra">
+			<div class="barra">
+				<img src="http://172.18.1.143:8002/wp-content/themes/steffany/nyan.gif" class="logo" />
+				<img src="http://172.18.1.143:8002/wp-content/themes/steffany/logo.png" class="logo2" />				
+			</div>
+		</div>		
